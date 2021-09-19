d2156abd6f1a5dbda798f5b3a3de28238d340a9f
Герман <german@MacBook-Air-German.local>
2021-09-19 22:43:32 +0300

    new version

--- iterator_file.py
+++ iterator_file.py
@@ -1,71 +1,41 @@
-import requests
-import json  
+import json
 import hashlib
-url = 'https://raw.githubusercontent.com/mledoze/countries/master/countries.json'
-file_one = 'countries_links.txt'
 
 
+class CountryLinks:
 
-#1
-class MyIterator:
-    def __init__(self, url, file_one):
-        self.url = url
-        self.file_one = file_one
+	WIKI_URL = 'https://en.wikipedia.org/wiki/'
 
+	def __init__(self, countries_file_path: str):
+		with open(countries_file_path) as file:
+			countries_data = json.load(file)
+			country_names = (country['name']['official'] for country in countries_data)  # генератор возвращающий
+			                                                                           # имена стран
+			self.countries_names_iter = iter(country_names)  # итератор по именам стран
 
-    def __iter__(self):
-        self.making_links()
-        self.line = None
+	def __iter__(self):
+		return self
 
-    def __next__(self):
-        if not self.new_data:
-            raise StopIteration
-        for key in self.new_data:
-            country_name = key['name']['official']
-            wiki_country_link = f'https://en.wikipedia.org/wiki/{country_name}'
-            self.line = f'{country_name} — {wiki_country_link}\n'    
-            return self.line
-            
+	def make_link(self, country_name: str):
+		country_name = country_name.replace(' ', '_')
+		return f'{self.WIKI_URL}{country_name}'
 
-		
-
-    
-    # def making_links(self):
-    #     with open(self.file_one, 'w') as file:
-    #         data = requests.get(self.url, stream=True)
-    #         new_data =  data.json()
-    #         for key in new_data:
-    #             country_name = key['name']['official']
-    #             wiki_country_link = f'https://en.wikipedia.org/wiki/{country_name}'
-    #             line = f'{country_name} — {wiki_country_link}\n'
-    #             file.write(line) 
-
-    def making_links(self):
-        data = requests.get(self.url, stream=True)
-        self.new_data = data.json()
-
-                             
+	def __next__(self):
+		country_name = next(self.country_names_iter)                               
+		return f'{country_name} - {self.make_link(country_name)}'
 
 
-iterator = MyIterator('https://raw.githubusercontent.com/mledoze/countries/master/countries.json', 'countries_links.txt')
-
-for line in iterator:
-    with open(file_one, 'w') as file:
-        file.write(line) 
-    
-        
+def get_hash(path: str):
+	with open(path, 'rb') as file:
+		for line in file:
+			yield hashlib.md5(line).hexdigest()
 
 
-
-#2
-
-# def MyGenerator(file_path):
-#     with open(file_path) as file:
-#         for line in file:
-#             hash_line = hashlib.md5(line.encode)
-#             yield hash_line.hexdigest()
-
-# for line in MyGenerator('countries_links.txt'):
-#     print(line)
+if __name__ == '__main__':
+	with open('country_names.txt', 'w') as country_names_file:
+		for country_link in CountryLinks('countries.json'): 
+			country_names_file.write(f'{country_link}\n')  
+	for hash_string in get_hash('country_names.txt'):
+		print(hash_string)
   
             
